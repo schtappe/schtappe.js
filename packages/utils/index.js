23b2e4fdb20f1176ad40ba9ef270d9e26ea047d5
Arvin Sevilla <sevillaarvin@gmail.com>
2024-03-15 13:36:22 +0800

    feat(utils): add capitalize and functionalize

--- packages/utils/index.js
+++ packages/utils/index.js
@@ -1,1 +1,11 @@
+export const capitalize = (value) => {
+        value = String(value)
+        return value.charAt(0).toLocaleUpperCase() + value.slice(1)
+}
+
+export const functionalize = (maybeFn) =>
+        typeof maybeFn == "function"
+                ? maybeFn
+                : () => maybeFn
+
 export const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
