ffb4957701e8d5ca170f2a1841f52043904af745
Arvin Sevilla <sevillaarvin@gmail.com>
2024-03-15 14:29:45 +0800

    feat(utils): add identity and empty functions

--- packages/utils/index.js
+++ packages/utils/index.js
@@ -3,10 +3,30 @@ export const capitalize = (value) => {
         return value.charAt(0).toLocaleUpperCase() + value.slice(1)
 }
 
+const _empty = new Map([
+        // primitives
+        [ Array, [] ],
+        [ Boolean, false ],
+        [ Function, () => {} ],
+        [ Number, 0 ],
+        [ Object, {} ],
+        [ String, "" ],
+        [ Symbol, Symbol() ],
+
+        // non-primitives
+        [ File, new File([""], "") ],
+])
+export const empty = (type) => {
+        if (!_empty.has(type)) throw new Error("Unknown empty type")
+        return _empty.get(type)
+}
+
 export const functionalize = (maybeFn) =>
         typeof maybeFn == "function"
                 ? maybeFn
                 : () => maybeFn
+
+export const identity = (v) => (v)
 
 export const pick = (props = []) => (object = {}) => {
         return props.reduce((result, prop) => {
--- packages/utils/tests/index.js
+++ packages/utils/tests/index.js
@@ -1,4 +1,15 @@
+// TODO(aes): create a test runner
+
 import * as Utils from "../index.js"
 
-let result = Utils.pick(["foo"])({ foo: 1, bar: 2})
+let result
+
+result = Utils.pick(["foo"])({ foo: 1, bar: 2})
 console.assert(result.foo == 1 && result.bar == undefined)
+
+result = Utils.empty(Boolean)
+console.assert(result == false)
+result = Utils.empty(File)
+console.assert("size" in result)
+
+console.log("done!")
