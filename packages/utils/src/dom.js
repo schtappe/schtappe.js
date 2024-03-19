176253d0a55bc1cd3c235474bfbe54a823d4e5b3
Arvin Sevilla <sevillaarvin@gmail.com>
2024-03-19 21:10:13 +0800

    feat(utils): add download blob function

---
+++ packages/utils/src/dom.js
@@ -0,0 +1,20 @@
+// TODO(aes): how to test this file if it depends on dom?
+
+// https://blog.logrocket.com/programmatic-file-downloads-in-the-browser-9a5186298d5c/
+export const downloadBlob = (blob, filename) => {
+        const url = URL.createObjectURL(blob)
+        const a = document.createElement("a")
+        a.href = url
+        a.download = filename || "download"
+
+        const clickHandler = () => {
+                setTimeout(() => {
+                        URL.revokeObjectURL(url)
+                        this.removeEventListener("click", clickHandler)
+                }, 150)
+        }
+        a.addEventListener("click", clickHandler, false)
+        a.click()
+
+        return a
+}
