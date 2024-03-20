8aaf7cd27d34c1a0a5ccf848ebfa8b283b5c7763
Arvin Sevilla <sevillaarvin@gmail.com>
2024-03-21 01:37:20 +0800

    chore(devenv): add language servers

--- devenv.nix
+++ devenv.nix
@@ -1,6 +1,11 @@
 { pkgs, ... }:
 
 {
+  packages = with pkgs;
+    [ vscode-langservers-extracted
+      nodePackages.typescript-language-server
+    ];
+
   languages.javascript = {
     enable = true;
     npm.enable = true;
